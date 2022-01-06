550c2615561fdc555c912297a148c4117400485c
John Orthoefer <john@orthoefer.org>
2022-01-06 00:29:48 -0500

    made things more modular

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
    "io"
    "fmt"
+   "log"
    "net/http"
    "net/url"
    "strconv"
@@ -10,125 +11,20 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
    "github.com/alecthomas/kong"
+   "gopkg.in/yaml.v2"
 )
 
-/*
-https://support.weather.com/s/article/PWS-Upload-Protocol?language=en_US
+var (
+   gauge GaugeMap
+)
 
-2021/12/28 03:07:32 map["ID":["KMAFRAMI83"] "PASSWORD":["LaC7PgHc"] "action":["updateraw"] "baromin":["29.75"] "dailyrainin":["0.00"] "dateutc":["now"] "dewptf":["31"] "hubbattery":["normal"] "humidity":["98"] "id":["24C86E0CE519"] "mt":["5N1"] "rainin":["0.00"] "realtime":["1"] "rssi":["31"] "rtfreq":["2.5"] "sensor":["06086M"] "sensorbattery":["normal"] "softwaretype":["myAcuRite"] "tempf":["31.3"] "winddir":["180"] "windgustmph":["0"] "windspeedavgmph":["0"] "windspeedmph":["0"]]
+/* 
+https://support.weather.com/s/article/PWS-Upload-Protocol
 
-ID: Wunderground ID
-action: Always "updateraw"
-baromin: [barometric pressure inches]
-dailyrainin: [rain inches so far today in local time]
-dewptf: [F outdoor dewpoint F]
-hubbattery: 
-humidity: [% outdoor humidity 0-100%]
-id: Units MAC Address
-mt: 
-rainin: [rain inches over the past hour)]
-realtime: RapidFire Updates ["1" = enabled]
-rssi: Radio power
-rtfreq: RapidFire Updates Frequency [2.5s]
-sensor:["06086M"] 
-sensorbattery:["normal"] 
-softwaretype:["myAcuRite"] 
-tempf:[F outdoor temperature] ["31.3"] 
-winddir: [0-360 instantaneous wind direction] ["180"] 
-windgustmph:[mph current wind gust, using software specific time period] ["0"] 
-windspeedavgmph: over 2 minutes Maybe? ["0"] 
-windspeedmph: [mph instantaneous wind speed] ["0"]
-
+All the fields are defined on the Weather.com Website.
 */
 
-type Gauges_Config struct {
-   Option struct {
-      Name string
-      Arg  string
-      Value string
-      Help string
-   }
-}
-
-var (
-   gauges map[string]interface{}
-	dateutc = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_dateutc",
-			Help: "second resolution Date-time stamp",
-		},
-		[]string{"id"},
-	)
-	winddir = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_winddir",
-			Help: "0-360 instantaneous wind direction",
-		},
-		[]string{"id"},
-	)
-	windspeedmph = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_windspeedmph",
-			Help: "mph instantaneous wind speed",
-		},
-		[]string{"id"},
-	)
-	windgustmph = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_windgustmph",
-			Help: "mph current wind gust, using software specific time period",
-		},
-		[]string{"id"},
-	)
-	windgustdir = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_windgustdir",
-			Help: "0-360 using software specific time period",
-		},
-		[]string{"id"},
-	)
-	windspeedavgmph = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_windspeedavgmph",
-			Help: "mph 2 minute average wind speed mph",
-		},
-		[]string{"id"},
-	)
-	tempf = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_tempf",
-			Help: "F outdoor temperature",
-		},
-		[]string{"id"},
-	)
-	humidity = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_humidity",
-			Help: "percent outdoor humidity 0-100",
-		},
-		[]string{"id"},
-	)
-	baromin = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_baromin",
-			Help: "barometric pressure inches",
-		},
-		[]string{"id"},
-	)
-	dewptf = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_dewptf",
-			Help: "F outdoor dewpoint F",
-		},
-		[]string{"id"},
-	)
-	rainin = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "weather_rainin",
-			Help: "rain inches over the past hour",
-		},
-		[]string{"id"},
-	)
+/*
 	dailyrainin = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "weather_dailyrainin",
@@ -136,7 +32,7 @@ var (
 		},
 		[]string{"id"},
 	)
-)
+*/
 
 var config struct {
    Upstream string  `help:"URL to Receive" default:"https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"`
@@ -188,34 +84,35 @@ func weather(w http.ResponseWriter, req *http.Request) {
    fmt.Printf("Upload Return:%s Body:%s\n", resp.Status, strconv.Quote(string(body)))
    fmt.Fprintf(w, string(body))
    v := req.URL.Query()
+   //fmt.Printf("%q\n", v)
 
+   if _, ok := v["ID"]; !ok {
+      fmt.Printf("No wunderground.com ID/Login (REQUIRED)")
+      return
+   }
    id := v["ID"][0]
-   label := prometheus.Labels{"id":id}
-   dateutc.With(label).Set(float64(getTimestamp(v["dateutc"][0])))
-   tempf.With(label).Set(getFloat(v["tempf"][0]))
-   humidity.With(label).Set(getFloat(v["humidity"][0]))
-   baromin.With(label).Set(getFloat(v["baromin"][0]))
-   dewptf.With(label).Set(getFloat(v["dewptf"][0]))
-   rainin.With(label).Set(getFloat(v["rainin"][0]))
-   dailyrainin.With(label).Set(getFloat(v["dailyrainin"][0]))
-   winddir.With(label).Set(getFloat(v["winddir"][0]))
-   windgustmph.With(label).Set(getFloat(v["windgustmph"][0]))
-   windspeedavgmph.With(label).Set(getFloat(v["windspeedavgmph"][0]))
-   windspeedmph.With(label).Set(getFloat(v["windspeedmph"][0]))
-}
 
-func init() {
-	prometheus.MustRegister(dateutc)
-	prometheus.MustRegister(tempf)
-	prometheus.MustRegister(humidity)
-	prometheus.MustRegister(baromin)
-	prometheus.MustRegister(dewptf)
-	prometheus.MustRegister(rainin)
-	prometheus.MustRegister(dailyrainin)
-	prometheus.MustRegister(winddir)
-	prometheus.MustRegister(windgustmph)
-	prometheus.MustRegister(windspeedavgmph)
-	prometheus.MustRegister(windspeedmph)
+   if _, ok := v["PASSWORD"]; !ok {
+      fmt.Printf("No wunderground.com Password (REQUIRED)")
+      return
+   }
+   //passwd := v["PASSWORD"][0]
+
+   if _, ok := v["dateutc"]; !ok {
+      fmt.Printf("No UTC Date (REQUIRED)")
+      return
+   }
+   dateutc := v["dateutc"][0]
+
+   label := prometheus.Labels{"id":id}
+   gauge.Tags["dateutc"].Value.With(label).Set(float64(getTimestamp(dateutc)))
+   delete(v, "dateutc")
+   for k, val := range v {
+      if _, ok := gauge.Tags[k]; !ok {
+         continue
+      }
+      gauge.Tags[k].Value.With(label).Set(float64(getFloat(val[0])))
+   }
 }
 
 func main() {
@@ -235,6 +132,31 @@ func main() {
    fmt.Printf("Path: %s\n", config.Path)
    fmt.Printf("Metrics: %s\n", config.Metrics)
    fmt.Printf("Upload URL: %s\n", config.upURL.String())
+
+   err = yaml.Unmarshal([]byte(yamlDefault), &gauge)
+   if err != nil {
+      log.Fatalf("YAML Parse Error, %s", err)
+   }
+
+   for key, v := range gauge.Tags {
+      if v.Value != nil {
+         continue
+      }
+      if v.Name == "" {
+         gauge.Tags[key].Name = config.Prefix + key
+      }
+      gauge.Tags[key].Value = prometheus.NewGaugeVec(
+                  prometheus.GaugeOpts{
+                     Name: v.Name,
+                     Help: v.Help,
+                  },
+                  []string{"id"},
+               )
+      prometheus.MustRegister(v.Value)
+      for _, alias := range v.Alias {
+         gauge.Tags[alias] = v
+      }
+   }
 
    http.HandleFunc(config.Path, weather)
    http.Handle(config.Metrics, promhttp.Handler())
